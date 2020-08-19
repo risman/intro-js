5cffedf5faa2ed28bdd4250b420807e56aa87af1
Risman <Risman@Rishis-MBP.lan>
2020-08-19 22:48:17 +0300

    fetch

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 console.log('That was it');
 
+var a = 1;
+var b = 2;
+var c = a+b;
+console.log(c);
+if (a < b) {
+    
+    console.log('a is lesser than b');
+    console.log(a + ' is lesser than ' + b);
+}
+
 
 
